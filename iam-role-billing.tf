2af9d3adc00e4b4716aaa76ee071c9621748a001
Allan Denot <adenot@gmail.com>
2019-08-20 18:28:07 +1000

    Initial commit; Module that creates billing role in master account

---
+++ iam-role-billing.tf
@@ -0,0 +1,27 @@
+data "aws_iam_policy_document" "assume_role_billing" {
+  statement = {
+    principals = {
+      type = "AWS"
+
+      identifiers = [
+        "arn:aws:iam::${var.idp_account_id}:root",
+      ]
+    }
+
+    actions = [
+      "sts:AssumeRole",
+    ]
+  }
+}
+
+resource "aws_iam_role" "billing" {
+  name                 = "${var.org_name}-billing"
+  assume_role_policy   = "${data.aws_iam_policy_document.assume_role_billing.json}"
+  max_session_duration = "${var.role_max_session_duration}"
+}
+
+resource "aws_iam_policy_attachment" "billing" {
+  name       = "billing-attachment"
+  roles      = ["${aws_iam_role.billing.name}"]
+  policy_arn = "arn:aws:iam::aws:policy/job-function/Billing"
+}
