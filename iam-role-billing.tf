cedba3f9e87540bb5165ca8f6883f648fdffd9bd
Allan Denot <adenot@gmail.com>
2019-11-13 15:20:03 +1100

    Fixing billing attachment

--- iam-role-billing.tf
+++ iam-role-billing.tf
@@ -20,8 +20,7 @@ resource "aws_iam_role" "billing" {
   max_session_duration = "${var.role_max_session_duration}"
 }
 
-resource "aws_iam_policy_attachment" "billing" {
-  name       = "billing-attachment-${var.idp_account_id}"
-  roles      = ["${aws_iam_role.billing.name}"]
+resource "aws_iam_role_policy_attachment" "billing" {
+  role       = "${aws_iam_role.billing.name}"
   policy_arn = "arn:aws:iam::aws:policy/job-function/Billing"
 }
