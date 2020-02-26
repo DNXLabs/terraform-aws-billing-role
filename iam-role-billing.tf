6009b202abb9410af576a7fdc8458205d5f373ab
Arthur Diniz <arthurbdiniz@gmail.com>
2020-02-26 14:20:10 +1100

    Adds terraform 0.12 support
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- iam-role-billing.tf
+++ iam-role-billing.tf
@@ -16,11 +16,11 @@ data "aws_iam_policy_document" "assume_role_billing" {
 
 resource "aws_iam_role" "billing" {
   name                 = "${var.org_name}-billing"
-  assume_role_policy   = "${data.aws_iam_policy_document.assume_role_billing.json}"
-  max_session_duration = "${var.role_max_session_duration}"
+  assume_role_policy   = data.aws_iam_policy_document.assume_role_billing.json
+  max_session_duration = var.role_max_session_duration
 }
 
 resource "aws_iam_role_policy_attachment" "billing" {
-  role       = "${aws_iam_role.billing.name}"
+  role       = aws_iam_role.billing.name
   policy_arn = "arn:aws:iam::aws:policy/job-function/Billing"
 }
